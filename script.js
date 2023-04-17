0d2062dae1d295d15e87e7523b979abfc43c5d2d
Viktor Stvolov <mr.stwolow@yandex.ru>
2023-04-17 14:58:27 +0300

    syntactic sugar

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-function closeRemove(item) {
-  item.classList.remove('popup_opened');
-}
+const popupEdit = document.querySelector('#popup-edit');
+const popupNew = document.querySelector('#popup-post');
+const popupImg = document.querySelector('#popup-img');
 
-function createCard(sourseValue, textValue)
-{
-  const popupImg = document.querySelector('#popup-img');
-  const postTemplate = document.querySelector('#post-template').content;
-  const postElement = postTemplate.querySelector('.post').cloneNode(true);
-  postElement.querySelector('.post__img').src = sourseValue;
-  postElement.querySelector('.post__text').textContent = textValue;
+const popupForm = popupEdit.querySelector('.popup__form');
+const popupFormNewCard = popupNew.querySelector('.popup__form');
+const profile = document.querySelector('.profile');
+const profileInfo = profile.querySelector('.profile__info');
+const profileSubtitle = profile.querySelector('.profile__subtitle');
+const profileButton = profile.querySelector('.profile__button');
+const profileEdit = document.querySelector('.profile__edit-info');
+const groupList = document.querySelector('.group__list');
+const popupCloseAll = document.querySelectorAll('.popup__close');
+const postTemplate = document.querySelector('#post-template').content;
 
-  postElement.querySelector('.post__img-heart').addEventListener('click', function (evt){
-    const evtHeart = evt.target;
-    evtHeart.classList.toggle('post__img-heart_active');
-  });
-  postElement.querySelector('.post__trash').addEventListener('click', function() {
-    const delPost = postElement.querySelector('.post__trash').closest('.post');
-    delPost.remove();
-  });
-
-  postElement.querySelector('.post__img').addEventListener('click', function () {
-    popupImg.querySelector('.popup__img-click').src = postElement.querySelector('.post__img').src;
-    popupImg.querySelector('.popup__text-click').textContent = textValue;
-    popupImg.classList.add('popup_opened');
-  });
-
-  popupImg.querySelector('.popup__close').addEventListener('click', function () {
-    closeRemove(popupImg);
-  })
-  return postElement;
-} 
+const surname = popupEdit.querySelector('.popup__input_type_name');
+const hobby = popupEdit.querySelector('.popup__input_type_hobby');
+surname.value = profileInfo.textContent;
+hobby.value = profileSubtitle.textContent;
 
 function openPopup(item) {
   item.classList.add('popup_opened');
@@ -39,46 +26,75 @@ function closePopup(item) {
   item.classList.remove('popup_opened');
 }
 
-const popupEdit = document.querySelector('#popup-edit');
-const popupNew = document.querySelector('#popup-post');
-const profile = document.querySelector('.profile');
-const profileEdit = document.querySelector('.profile__edit-info');
-const groupList = document.querySelector('.group__list');
+function createCard(cardObj)
+{
+  const postElement = postTemplate.querySelector('.post').cloneNode(true);
+  const postImg = postElement.querySelector('.post__img');
+  const postText = postElement.querySelector('.post__text');
+  const postImgHeart = postElement.querySelector('.post__img-heart');
+  const postTrash = postElement.querySelector('.post__trash');
+  const popupImgClick = popupImg.querySelector('.popup__img-click');
+  const popupTextClick = popupImg.querySelector('.popup__text-click');
+
+  postImg.src = cardObj.link;
+  postImg.alt = cardObj.name;
+  postText.textContent = cardObj.name;
+
+  postImgHeart.addEventListener('click', function (evt) {
+    const evtHeart = evt.target;
+    evtHeart.classList.toggle('post__img-heart_active');
+  });
+  postTrash.addEventListener('click', function() {
+    postElement.remove();
+  });
+
+  postImg.addEventListener('click', function () {
+    popupImgClick.src = cardObj.link;
+    popupImgClick.alt = cardObj.name;
+    popupTextClick.textContent = cardObj.name;
+    popupImg.classList.add('popup_opened');
+  });
+  return postElement;
+} 
+
+
+
+initialCards.forEach (function (item, index) {
+  const newPost = createCard(item);
+  groupList.append(newPost);
+})
 
 profileEdit.addEventListener('click', function () {
   openPopup(popupEdit);
 });
-popupEdit.querySelector('.popup__close').addEventListener('click', function () {
-  closePopup(popupEdit);
-});
-popupEdit.querySelector('.popup__form').addEventListener('submit', function (evt) {
+
+
+
+popupForm.addEventListener('submit', function (evt) {
   evt.preventDefault();
-  const name = popupEdit.querySelector('.popup__input_type_name');
-  const hobby = popupEdit.querySelector('.popup__input_type_hobby');
-  profile.querySelector('.profile__info').textContent = name.value;
-  profile.querySelector('.profile__subtitle').textContent = hobby.value;
+  profileInfo.textContent = surname.value;
+  profileSubtitle.textContent = hobby.value;
   closePopup(popupEdit);
-  name.value = '';
-  hobby.value = '';
 })
-profile.querySelector('.profile__button').addEventListener('click', function () {
+profileButton.addEventListener('click', function () {
   openPopup(popupNew);
-})
-popupNew.querySelector('.popup__close').addEventListener('click', function () {
-  closePopup(popupNew);
-})
-popupNew.querySelector('.popup__form').addEventListener('submit', function (evt) {
+});
+
+popupFormNewCard.addEventListener('submit', function (evt) {
   evt.preventDefault();
-  const text = popupNew.querySelector('.popup__input_type_post');
-  const link = popupNew.querySelector('.popup__input_type_link');
-  const newCard = createCard(link.value, text.value);
+  typePost = popupNew.querySelector('.popup__input_type_post');
+  typeLink = popupNew.querySelector('.popup__input_type_link');
+  const cardObj = {name: typePost.value, link: typeLink.value};
+  const newCard = createCard(cardObj);
   groupList.prepend(newCard);
-  text.value = '';
-  link.value = '';
+  typePost.value = '';
+  typeLink.value = '';
   closePopup(popupNew); 
 })
 
-initialCards.forEach (function (item, index) {
-  const newPost = createCard(item.link, item.name);
-  groupList.append(newPost);
+
+popupCloseAll.forEach(function (button) {
+  const buttonsPopup = button.closest('.popup');
+  button.addEventListener('click', function () {
+    closePopup(buttonsPopup)});
 })
